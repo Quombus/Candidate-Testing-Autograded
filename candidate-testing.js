8148be996e3dc28150cc0eb9a6c9f601ee6bc92d
Quombus <beckettrichardmatthew@gmail.com>
2024-06-20 12:38:15 -0500

    Assignment 1 attempt

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,38 +3,58 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question 
+let correctAnswer
+let candidateAnswer
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
-
+let questions = ['Who was the first American woman in space' 
+  ,'True or false: 5 kilometer == 5000 meters', '(5 + 3)/2 * 10 = ',`Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2`, 'What is the minimum crew size for the ISS'];
+let correctAnswers = `Sally Ride, true, 40, Trajectory, 3`;
+let candidateAnswers = [];
+let candidatePrompt= [];
+let points = 0;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName = input.question("Candidate Name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  for (let i = 0; i < questions.length; i++) {
+      candidatePrompt = input.question( (i + 1) + ') ' + questions[i] + "? : ").toUpperCase().trim();
+      candidateAnswers = candidateAnswers.concat(candidatePrompt);
+  }
+  correctAnswers = correctAnswers.toUpperCase().split(', ');
+  for (let i = 0; i < questions.length; i++) {
+      if (candidateAnswers[i] === correctAnswers[i]) { 
+          points = points + 1;
+      } 
+  }
 }
-
 function gradeQuiz(candidateAnswers) {
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
+  
 
-
-
+  
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
+  
 
+ grade = (points * 2) * 10;
+
+ console.log("<< Candidate recieved a total of " + points  + ". >>");
+if (points === 0 || points === 1  || points === 2 || points === 3 ) {
+  console.log("<< Candidate recieved a grade of " + grade + "%. Candidate failed. >>");
+ 
+} else { 
+    console.log("<< Candidate recieved a grade of " + grade + "%. candidate passed! >>");
+    
+}
 
   return grade;
 }
@@ -42,7 +62,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+   console.log('Hello, ' + candidateName + ' !');
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
